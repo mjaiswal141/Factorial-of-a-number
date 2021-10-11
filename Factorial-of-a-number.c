f957d674ef160b934d35a72d248c5ea6d93f1ba9
Manas Jaiswal <52818738+mjaiswal141@users.noreply.github.com>
2021-10-11 23:14:07 +0530

    Update Factorial-of-a-number.c

--- Factorial-of-a-number.c
+++ Factorial-of-a-number.c
@@ -1,12 +1,11 @@
 //Factorial-of-a-number
 #include <stdio.h>
-int main() {
+int main() 
+{
     int n, i;
     unsigned long long fact = 1;
     printf("Enter an integer: ");
     scanf("%d", &n);
-
-    // shows error if the user enters a negative integer
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
     else {
@@ -15,6 +14,5 @@ int main() {
         }
         printf("Factorial of %d = %llu", n, fact);
     }
-
     return 0;
 }
